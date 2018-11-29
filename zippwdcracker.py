3f35c67e8adeec198b9a33241f9498b3b4cc664a
Kartikey Sharma <36727029+kartikeysharma34@users.noreply.github.com>
2018-11-29 11:37:55 +0530

    Add files via upload

---
+++ zippwdcracker.py
@@ -0,0 +1,40 @@
+import zipfile
+import optparse
+from threading import Thread
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        print (' [+] Found password' + password + '\n')
+    except:
+        pass
+
+def main():
+    parser = optparse.OptionParser("usage%prog " + \
+                                   "-f <zipfile> -d <dictionary>")
+    parser.add_option('-f' , dest = 'zname' , type = 'string',\
+                      help = 'specify zip file')
+    parser.add_option('-d', dest = 'dname' , type = 'string',\
+                      help = 'speciify dictionary file')
+
+    (option, args) = parser.parse_args()
+    if (option.zname == None) | (option.dname == None):
+        print (parser.usage)
+        exit(0)
+
+    else:
+        zname = options.zname
+        dname = options.dname
+        zFile = zipfile.ZipFile(zname)
+        passFile = open(dname)
+        for line in passFile.readlines():
+            password = line.strip('\n')
+            t = Thread(target=extractFile, args=(zFile, password))
+            t.start()
+            
+        
+
+    
+
+if __name__ == '__main__':
+    main()
+    
